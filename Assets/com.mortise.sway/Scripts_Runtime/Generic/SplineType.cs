04569e74952dab52e7b50e938a2c8884b34926aa
onovich <hjn1110@163.com>
2024-04-09 03:32:39 +0800

    <misc> modify: SplineType

--- Assets/com.mortise.sway/Scripts_Runtime/Generic/SplineType.cs
+++ Assets/com.mortise.sway/Scripts_Runtime/Generic/SplineType.cs
@@ -1,10 +1,10 @@
 namespace MortiseFrame.Sway {
 
-    public enum SplineType {
-        Bezier,
-        CatmullRom,
-        Hermite,
-        BSpline
+    public enum SplineType : int {
+        Bezier = 0,
+        CatmullRom = 1,
+        Hermite = 2,
+        BSpline = 3
     }
 
 }
